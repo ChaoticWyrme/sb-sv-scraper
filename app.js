ea16b6a57ced39448c9b085a4fdb6a27a02125f8
Mason Osborn <mason.osborn@gmail.com>
2017-04-27 10:45:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ var url = 'https://forums.sufficientvelocity.com/threads/e-l-f-extraterrestrial-
 JSDOM.fromURL(url).then(dom => {
   console.log(dom.serialize());
 });
+
+function fromFragment(dom) { // retrieves element hash links to
+  return document.getElementById(dom.location.hash.substr(1));
+}
+
+function getAuthor(author, dom) {
+  return dom.querySelectorAll("li[data-author='" + author + "']"); // selects every list item
+}
+
+function getPostContent(post) { // takes input from getAuthor
+  var content = post.getElementsByClassName("messageText")[0];
+  let mark = content.getElementsByClassName("messageTextEndMarker");
+  if(mark.length > 0) content.removeChild(mark[0]); // if there is an end text marker remove it
+  return content;
+}
+
+function parseThread(url,callback) {
+  url = new URL(url);
+  let parser = url.host;
+}

d178eb33f9636a6458db68c09c85849a9dc5d3ac
Mason Osborn <mason.osborn@gmail.com>
2017-05-04 15:08:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-#!/env/bin/node
+#!/usr/bin/env node
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const fs = require('fs');
 // DEBUG START
 var url = 'https://forums.sufficientvelocity.com/threads/e-l-f-extraterrestrial-lifeform.30454/';
-
 // DEBUG END
 
 function XFParse(url) { // XenForo Parser
@@ -17,38 +16,42 @@ function XFParse(url) { // XenForo Parser
   this.fromFragment = () => { // retrieves element hash links to
     return document.getElementById(dom.location.hash.substr(1));
   }
-  this.getAuthor = (author) => {
+  this.getAuthor = author => {
     return dom.querySelectorAll("li[data-author='" + author + "']"); // selects every list item
   }
-  this.resetDOM() {
+  this.resetDOM = () => {
     JSDOM.fromURL(url).then((doc) => {
       dom = doc;
     });
   }
-  this.getPostContent (post) => { // takes input from getAuthor
+  this.getPostContent = (post) => { // takes input from getAuthor
     var content = post.getElementsByClassName("messageText")[0];
     let mark = content.getElementsByClassName("messageTextEndMarker");
     if(mark.length > 0) content.removeChild(mark[0]); // if there is an end text marker remove it
     return content;
   }
-  this.getThreadmarks() {
+  this.getThreadmarks = () => {
+    var posts;
     JSDOM.fromURL(url.href.replace(/\/?$/,'reader')).then((doc) => {
       dom = doc;
-      
+      posts = this.getAllPosts();
+      this.resetDOM();
+      return posts;
     });
   }
+  this.getAllPosts() {
+    var posts = []; // array of strings of posts
+    posts.push(dom.getElementsByClassName('message').forEach(this.getPostContent()));
+    return posts;
+  }
 }
-this.getAllPosts() {
-  var posts = []; // array of strings of posts
-  posts.append(dom.getElementsByClassName('message').forEach(this.getPostContent()));
-}
-this.parseThread(url,callback) { // will fallback on default parsers if no callback
+function parseThread(url,callback) { // will fallback on default parsers if no callback
   url = new URL(url);
   if(typeof callback !== 'function') {
     var parser = url.host;
     if(parser==='forums.spacebattles.com' || parser==='forums.sufficientvelocity.com') {
       parser = new XFParse(url);
-      fs.writeFile
+      fs.writeFileSync('test.txt',parser.getThreadmarks());
     }
   } else {
     parser = new callback(url);
